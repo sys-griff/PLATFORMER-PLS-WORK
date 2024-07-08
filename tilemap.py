9694eafc7b7a9e31e1ef3de75441e17dffd640f1
sys-griff <griffinromine29@gmail.com>
2024-07-08 13:28:26 -0700

    realnameshittibangcomalestshootabang

--- tilemap.py
+++ tilemap.py
@@ -1,5 +1,6 @@
 import pygame
 import json
+from tile import Tile
 
 class Tilemap:
     def __init__(self, map_data, tileset):
@@ -8,10 +9,13 @@ class Tilemap:
         self.tile_size = self.map_data['tileSize']
         self.map_width = self.map_data['mapWidth']
         self.map_height = self.map_data['mapHeight']
+        self.tiles = []
 
     def load_map(self):
         print(self.map_data['layers'][0]['tiles'][0])
 
         for layer in self.map_data['layers']:
-            for tiles in layer['tiles']:
-                print(tiles)
+            for tile in layer['tiles']:
+                x = tile[x]
+                y = tile[y]
+                Tile("temp", x, y)
